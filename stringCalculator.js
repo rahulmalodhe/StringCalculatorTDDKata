ad34068eb713c38b3c5f70f243979351aa9a602a
Rahul Malodhe S <malodhe18@gmail.com>
2024-06-21 19:48:16 +0530

    checking Single number

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,8 @@
 function add(numbers) {
   if (numbers == "") {
     return 0;
+  } else if (!numbers.includes(",")) {
+    return parseInt(numbers);
   }
 }
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,8 @@ import add from "./stringCalculator";
 test('should return 0 for an empty String', ()=>{
     expect( add("")).toBe(0)
 })
+
+test('should return the number itself for a single number', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+});
