db81cd5291a256eb7dee2d82e5974dd23323efa7
Rahul Malodhe S <malodhe18@gmail.com>
2024-06-21 21:35:45 +0530

    using typescript for strict evaluation

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,22 +1,25 @@
-function add(numbers) {
+function add(numbers: string): number {
   if (numbers === "") {
     return 0;
   }
 
   let delimiter = ",";
   if (numbers.startsWith("//")) {
-    const delimiterIndx = numbers.indexOf("\n");
-    delimiter = numbers.substring(2, delimiterIndx);
-    numbers = numbers.substring(delimiterIndx + 1);
+    const delimiterEndIndex = numbers.indexOf("\n");
+    delimiter = numbers.substring(2, delimiterEndIndex);
+    numbers = numbers.substring(delimiterEndIndex + 1);
   }
 
   const regexDelimiter = new RegExp(`[${delimiter}\n]`, "g");
   numbers = numbers.replace(regexDelimiter, ",");
+
   const numArray = numbers.split(",");
+
   const negatives = numArray.filter((num) => parseInt(num) < 0);
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
   }
+
   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
